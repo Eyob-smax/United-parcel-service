0a1f5560bf3783d54d43adbe2172b59b6feff629
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-08-24 07:36:08 +0300

    Adding some additional languages

--- parcel and courier/frontend/src/i18n/i18n.ts
+++ parcel and courier/frontend/src/i18n/i18n.ts
@@ -12,6 +12,31 @@ import ru from "../locales/ru.json";
 import pt from "../locales/pt.json";
 import de from "../locales/de.json";
 import ja from "../locales/ja.json";
+import am from "../locales/am.json";
+import dutch from "../locales/dutch.json";
+import egyptian_Arabic from "../locales/egyptian_Arabic.json";
+import bhojpuri from "../locales/bhojpuri.json";
+import gu from "../locales/gu.json";
+import id from "../locales/id.json";
+import kn from "../locales/kn.json";
+import javanese from "../locales/javanese.json";
+import ko from "../locales/ko.json";
+import ml from "../locales/ml.json";
+import it from "../locales/it.json";
+import ne from "../locales/ne.json";
+import pa from "../locales/pa.json";
+import ro from "../locales/ro.json";
+import sindhi from "../locales/sindhi.json";
+import sw from "../locales/sw.json";
+import si from "../locales/si.json";
+import ta from "../locales/ta.json";
+import th from "../locales/th.json";
+import tr from "../locales/tr.json";
+import wp from "../locales/wp.json";
+import wuc from "../locales/wuc.json";
+import vi from "../locales/vi.json";
+import te from "../locales/te.json";
+import om from "../locales/om.json";
 
 const resources: Record<string, { translation: Record<string, unknown> }> = {
   en: { translation: en },
@@ -24,6 +49,31 @@ const resources: Record<string, { translation: Record<string, unknown> }> = {
   pt: { translation: pt },
   de: { translation: de },
   ja: { translation: ja },
+  am: { translation: am },
+  dutch: { translation: dutch },
+  egyptian_Arabic: { translation: egyptian_Arabic },
+  om: { translation: om },
+  te: { translation: te },
+  vi: { translation: vi },
+  wuc: { translation: wuc },
+  wp: { translation: wp },
+  tr: { translation: tr },
+  th: { translation: th },
+  ta: { translation: ta },
+  si: { translation: si },
+  sw: { translation: sw },
+  sindhi: { translation: sindhi },
+  gu: { translation: gu },
+  kn: { translation: kn },
+  javanese: { translation: javanese },
+  ko: { translation: ko },
+  ml: { translation: ml },
+  it: { translation: it },
+  ne: { translation: ne },
+  pa: { translation: pa },
+  ro: { translation: ro },
+  id: { translation: id },
+  bhojpuri: { translation: bhojpuri },
 };
 
 i18n
@@ -32,7 +82,42 @@ i18n
   .init({
     resources,
     fallbackLng: "en",
-    supportedLngs: ["en", "es", "zh", "hi", "ar", "fr", "ru", "pt", "de", "ja"],
+    supportedLngs: [
+      "en",
+      "es",
+      "zh",
+      "hi",
+      "ar",
+      "fr",
+      "ru",
+      "pt",
+      "de",
+      "ja",
+      "am",
+      "dutch",
+      "egyptian_Arabic",
+      "bhojpuri",
+      "gu",
+      "kn",
+      "javanese",
+      "ko",
+      "ml",
+      "it",
+      "ne",
+      "pa",
+      "ro",
+      "te",
+      "vi",
+      "wuc",
+      "wp",
+      "tr",
+      "th",
+      "ta",
+      "si",
+      "sw",
+      "sindhi",
+      "om",
+    ],
     interpolation: {
       escapeValue: false,
     },
